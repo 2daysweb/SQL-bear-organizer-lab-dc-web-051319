3361f926271ccde86a83938e82ef157b46030d4d
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-05-26 01:19:29 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO BEARS(name, age, gender, color, temperament, alive) VALUES("Abe", 1, "F", "Black", "Angry", "Dead");
+INSERT INTO BEARS(name, age, gender, color, temperament, alive) VALUES("Sam", 10, "F", "Black", "Angry", "Dead");
+INSERT INTO BEARS(name, age, gender, color, temperament, alive) VALUES("Kris", 10, "F", "Black", "Angry", "Dead");
+INSERT INTO BEARS(name, age, gender, color, temperament, alive) VALUES("Jack", 10, "F", "Black", "Angry", "Dead");
+INSERT INTO BEARS(name, age, gender, color, temperament, alive) VALUES("Jan", 10, "M", "Black", "Angry", "Dead");
+INSERT INTO BEARS(name, age, gender, color, temperament, alive) VALUES("Jip", 10, "M", "Black", "Angry", "Dead");
+INSERT INTO BEARS(name, age, gender, color, temperament, alive) VALUES("Skip", 10, "M", "Black", "Angry", "Dead");
+INSERT INTO BEARS(age, gender, color, temperament, alive) VALUES(10, "M", "Black", "Angry", "Dead");
