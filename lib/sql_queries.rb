b4edc7c52443f5197a0dfe602c1f1e0562ecc00f
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-05-26 01:22:11 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,7 +1,7 @@
 def selects_all_female_bears_return_name_and_age
     SELECT name, age FROM bears
-            WHERE gender="female"
-
+            WHERE gender="female";
+            
 def selects_all_bears_names_and_orders_in_alphabetical_order
   "Write your SQL query here"
 end
